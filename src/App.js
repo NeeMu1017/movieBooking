baf8eadf399f50107f8a70121568fb91bf272e3e
NeeMu1017 <nirs2435@outlook.com>
2023-03-13 19:19:48 +0530

    primereact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Home } from './features/movie/home/Home';
+import Signin from './features/user/signin/Signin';
+import Sigup from './features/user/signup/Sigup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Home/>
+     {/* <Signin/> */}
     </div>
   );
 }
